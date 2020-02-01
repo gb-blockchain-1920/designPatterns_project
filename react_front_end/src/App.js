c941fdf0800594332546edfe83e6d65b46c9b165
Luka Buzaladze <buzaladzeluka@gmail.com>
2020-02-01 01:00:39 -0500

    added navbar and routing to react app

--- react_front_end/src/App.js
+++ react_front_end/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
 
-function App() {
+import PurchasePage from "./components/PurchasePage";
+import BidPage from "./components/BidPage";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            BitBnB
+          </IconButton>
+          <Link to="/purchase">
+            <Button>Purchase Shares</Button>
+          </Link>
+          <Link to="/bid">
+            <Button>Bid</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route path="/purchase">
+          <PurchasePage />
+        </Route>
+        <Route path="/bid">
+          <BidPage />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
